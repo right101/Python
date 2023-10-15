70a05d4b7329f78c533d6158459cff3ace7d7903
right101 <raya.mogilev@gmail.com>
2023-10-15 17:14:27 -0500

    name generator added

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = raw_input("What's the name of the city you grew up in?\n")
+pet = raw_input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
